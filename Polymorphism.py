2f5ed356c385eb027ac4783de2e9a53628fe2170
arunkumar <arunboya08@gmail.com>
2026-07-29 14:42:25 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,36 @@
+#method overriding............
+
+
+# class Animal:
+#     def sound(self):
+#         print("Animal makes a sound")
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("Dog barks")
+
+# class Cat(Animal):
+#     def sound(self):
+#         print("Cat meows")
+
+# d = Dog()
+# c = Cat()
+
+# d.sound()
+# c.sound()
+
+
+####method overloading..........
+
+
+# class cal:
+#     def add(self, a, b):
+#         return a+b
+#     def add(self,a,b,c=0):
+#         return a+b+c+d
+
+# obj = cal()
+# print(obj.add(10,20,30,))
+
+
+
