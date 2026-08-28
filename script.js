af4f0e280a3338379b0634765f97224a4c817305
arunkumar <arunboya08@gmail.com>
2026-08-28 20:45:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// let a ={
+//     title: "pfsd",
+//     description: "full stack",
+//     triner: "name"
+// }
+
+// console.log(a['title']);
+// console.log(typeof(a));
+
+
+// let k = { name: "name" };
+// let y = k;
+
+// k.name = "naveen";
+// console.log(k);
+// console.log(y);
+
+
+
+// let course = ["pfsd", "jfsd", "mern"];
+// console.log(course);
+// console.log(typeof(course));
+// console.log(course[0]);
+// console.log(course[1]);
+
+// console.log(course[2]);
+
+
+
+
+
+
+
+// function createCourse(coursename){
+//     console.log('creating' + coursename);
+// }
+
+// var m = 10;
+// console.log(m)
+// createCourse('pfsd')
+// createCourse('jsfd')
+
+// var a = 100;
+// console.log(a);
+// console.log(this.a);
+// console.log(window.a);
+
+
+
+// {
+//     let z = 10;
+//     var o = 20;
+//     const v = 30;
+
+// }
+// console.log(z);
+// console.log(o);
+// console.log(v);
+
+
+
+
+
+function add(a,b){
+    return a+b
+}
+
+console.log(add);
+console.log(add(2,3));
