8975478015a5107c02c564aa5d3881fe3bef7979
arunkumar <arunboya08@gmail.com>
2026-09-09 11:00:12 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import Navbar from '../components/Navbar'
+import Footer from "../components/Footer"
+function Home() {
+  return (
+
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
